5ca419eed8ff78b876b0ab42557d8ec18cea71a3
Anthony-bird <61493124+Anthony-bird@users.noreply.github.com>
2020-12-02 23:51:52 +0800

    miaosha
    12月2日第一次迭代

---
+++ miaosha/src/main/java/com/miaoshaproject/error/EmBusinessError.java
@@ -0,0 +1,38 @@
+package com.miaoshaproject.error;
+
+public enum EmBusinessError implements CommonError {
+    //通用错误类型100001
+    PARAMETER_VALIDATION_ERROR(10001,"参数不合法"),
+    UNKNOW_ERROR(10002,"未知错误"),
+    //20000开头为用户相关错误
+    USER_NOT_EXIST(20001,"用户不存在"),
+    USER_LOGIN_FAIL(20002,"手机或密码不存在"),
+    USER_NOT_LOGIN(20003,"用户还未登陆"),
+    //30000开头为交易型错误
+    STOCK_NOT_ENOUGH(30001,"库存不足")
+    ;
+    ;
+
+    private int errorCode;
+    private String errmsg;
+    private EmBusinessError(int errCode,String errMsg) {
+        this.errorCode=errCode;
+        this.errmsg=errMsg;
+    }
+
+    @Override
+    public int getErrCode() {
+        return this.errorCode;
+    }
+
+    @Override
+    public String getErrMsg() {
+        return this.errmsg;
+    }
+
+    @Override
+    public CommonError setErrMsg(String errMsg) {
+        this.errmsg = errMsg;
+        return this;
+    }
+}
